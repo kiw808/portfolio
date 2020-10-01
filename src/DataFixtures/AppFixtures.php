c9dec2e3293e52cfa66bbb6df8f19f76fcc624e5
Pierre Muller <61626102+kiw808@users.noreply.github.com>
2020-10-01 21:08:34 +0200

    Pm entities (#3)
    * make user
    
    * make migration
    
    * user fixture
    
    * login form & auth
    
    * fix type hints

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager) : void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
