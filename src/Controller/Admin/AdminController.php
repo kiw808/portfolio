a5885c82734b55a607fd24bf6f70ad3e2073b545
Pierre Muller <pierremanu.muller@gmail.com>
2020-10-05 21:20:23 +0200

    admin index type hint

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -15,7 +16,7 @@ class AdminController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index()
+    public function index() : Response
     {
         return $this->render('admin/index.html.twig', [
         ]);
