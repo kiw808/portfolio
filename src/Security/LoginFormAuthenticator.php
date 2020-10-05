9fb0a5d8f7a27d99b64e61d135c5104f89a6356a
Pierre Muller <pierremanu.muller@gmail.com>
2020-10-06 00:20:02 +0200

    fix routes

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -100,7 +100,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
         // Success login redirection
-        return new RedirectResponse($this->urlGenerator->generate('home'));
+        return new RedirectResponse($this->urlGenerator->generate('front_home'));
     }
 
     protected function getLoginUrl()
