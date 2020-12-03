03418a04650a95a548ddbb22261d22392f06263f
Pierre Muller <61626102+kiw808@users.noreply.github.com>
2020-12-03 19:08:10 +0100

    Pm start front (#9)
    * display technos
    
    * frontpage base
    
    * composer update

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Front;
 
+use App\Repository\TechnologyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,11 +11,15 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="front_home")
+     * @param TechnologyRepository $technologyRepository
+     * @return Response
      */
-    public function index() : Response
+    public function index(TechnologyRepository $technologyRepository) : Response
     {
+        $technos = $technologyRepository->findAll();
+
         return $this->render('front/home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'technos' => $technos,
         ]);
     }
 }
