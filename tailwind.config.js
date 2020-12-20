80d9cdbc4c2f69f6a0aef203353b9ee6f5a8247f
Pierre Muller <pierremanu.muller@gmail.com>
2020-12-20 11:13:05 +0100

    navbar fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-    purge: [],
     theme: {
-        extend: {},
-    },
-    variants: {},
-    plugins: [],
+        extend: {
+            colors: {
+                'primary': '#f05454',
+                'secondary': '#30475e',
+                'dark': '#222831',
+                'light': '#e8e8e8',
+            }
+        }
+    }
 }
