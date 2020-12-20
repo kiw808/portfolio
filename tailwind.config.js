1699a0c1355ba46eb58afd45b8a65fcf41951cdf
Pierre Muller <pierremanu.muller@gmail.com>
2020-12-20 14:01:35 +0100

    simple hero section and nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
                 'dark': '#222831',
                 'light': '#e8e8e8',
             }
+        },
+        variants: {
+            extend: {
+                borderWidth: ['hover', 'focus'],
+            }
         }
     }
 }
